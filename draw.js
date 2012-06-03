6cb4d5acee919d1bd855a2275ac9378ccb18cd31
Peter Paul Elfferich <difosfor@gmail.com>
2012-06-04 01:39:38 +0200

    Added support for color mapping.

--- draw.js
+++ draw.js
@@ -11,11 +11,39 @@
 	// Variables
 	var resizeInput = document.getElementById('resize');
 	var zoomInput = document.getElementById('zoom');
-	var elem = document.getElementsByTagName('canvas')[0];
-	var context = elem.getContext('2d');
-	var canvas = context.canvas;
+	var canvas = document.getElementsByTagName('canvas')[0];
+	var context = canvas.getContext('2d');
+	var marbleCanvas = document.createElement('canvas');
+	var marbleContext = marbleCanvas.getContext('2d');
+	var marbleMapLength = 1000;
+
+	// Prepare marble color map
+	(function() {
+		marbleCanvas.width = marbleMapLength;
+		marbleCanvas.height = 10;
+
+		var grad = marbleContext.createLinearGradient(0, 0, marbleCanvas.width, 0);
+
+		grad.addColorStop(0, '#000000');
+		grad.addColorStop(0.07, '#806638');
+		grad.addColorStop(0.14, '#E3D6AC');
+		grad.addColorStop(0.18, '#806638');
+		grad.addColorStop(0.2, '#000000');
+		grad.addColorStop(0.6, '#8AA78D');
+		grad.addColorStop(0.95, '#F5FFEE');
+		grad.addColorStop(1.0, '#FFFFFF');
+
+		marbleContext.fillStyle = grad;
+		marbleContext.fillRect(0, 0, marbleCanvas.width, marbleCanvas.height);
+	}());
 
 	// Functions
+	var rgbas = []; // cache
+	function marble(v) {
+		var i = Math.floor(v * (marbleMapLength - 1));
+		return rgbas[i] || (rgbas[i] = marbleContext.getImageData(i, 0, 1, 1).data);
+	}
+
 	function draw(fn) {
 		if (fn) {
 			storage.fn = fn;
@@ -24,51 +52,74 @@
 		}
 
 		var f = noise[fn]; // resolve noise function name to function
+		var zoom = parseFloat(zoomInput.value);
+		var w = canvas.width;
+		var h = canvas.height;
+		var vs = new Array(w * h);
+		var start, x, y, sx, sy, rgba, i, di;
+		var imageData = context.createImageData(w, h);
+		var data = imageData.data;
 
 		if (f) {
 			console.log('draw(): ' + fn);
 		} else {
-			console.log('draw(): noise function undefined ' + fn);
+			context.drawImage(marbleCanvas, 0, 0, marbleCanvas.width, marbleCanvas.height, 0, 0, w, h);
+			console.log('draw(): noise function undefined: ' + fn);
 			return;
 		}
 
-		var resize = resizeInput.checked;
-		var zoom = parseFloat(zoomInput.value);
-		var w = resize ? window.innerWidth : canvas.width;
-		var h = resize ? window.innerHeight : canvas.height;
-		var imageData = context.createImageData(w, h);
-		var data = imageData.data;
-		var x, y;
-
-		canvas.width  = w;
-		canvas.height = h;
-
-		var start = Date.now();
-
+		// Calculate values
+		start = Date.now();
+		i = 0;
 		for (y = 0; y < h; y++) {
 			for (x = 0; x < w; x++) {
-				// scale input to normalized vector size
-				var sx = x / w * zoom;
-				var sy = y / h * zoom;
+				// Scale input to normalized vector size
+				sx = x / w * zoom;
+				sy = y / h * zoom;
 
-				// apply draw function
-				var v = f(sx, sy);
-
-				// convert the result from [-1, 1] to [0, 255]
-				v = (v * 0.5 + 0.5) * 255;
-
-				// store pixel in image data array
-				var i = (y * w + x) * 4;
-				data[i] = v; // red
-				data[i + 1] = v; // green
-				data[i + 2] = v; // blue
-				data[i + 3] = 255; // alpha
+				// Store draw function value
+				vs[i++] = f(sx, sy);
 			}
 		}
+		console.log('draw(): calculate ' + vs.length + ' values time: ' + (Date.now() - start));
 
+		// Normalize values (use full range of color map)
+		start = Date.now();
+		var minV = Number.MAX_VALUE;
+		var maxV = Number.MIN_VALUE;
+		for (i = 0; i < vs.length; i++) {
+			if (vs[i] < minV) {
+				minV = vs[i];
+			}
+			if (vs[i] > maxV) {
+				maxV = vs[i];
+			}
+		}
+		var vFac = 1 / (maxV - minV);
+		for (i = 0; i < vs.length; i++) {
+			vs[i] = (vs[i] - minV) * vFac;
+		}
+		console.log('draw(): normalize values time (' + minV + ' - ' + maxV + '): ' + (Date.now() - start));
+
+		// Prepare image data array
+		start = Date.now();
+		for (i = 0; i < vs.length; i++) {
+			// apply marble color mapping
+			rgba = marble(vs[i]);
+
+			// store pixel in image data array
+			di = i * 4;
+			data[di] = rgba[0];
+			data[di + 1] = rgba[1];
+			data[di + 2] = rgba[2];
+			data[di + 3] = rgba[3];
+		}
+		console.log('draw(): prepare image data time: ' + (Date.now() - start));
+
+		// Put image data
+		start = Date.now();
 		context.putImageData(imageData, 0, 0);
-
-		console.log('draw(): render time: ' + (Date.now() - start));
+		console.log('draw(): put image data time: ' + (Date.now() - start));
 	}
 
 	// Initialize inputs
@@ -79,20 +130,21 @@
 	draw();
 
 	// Bind inputs
+	function onResize() {
+		if (resizeInput.checked) {
+			canvas.width = window.innerWidth;
+			canvas.height = window.innerHeight;
+			draw();
+		}
+	}
+
 	resizeInput.addEventListener('change', function() {
 		console.log('onChange: resize: ' + resizeInput.checked);
 		storage.resize = resizeInput.checked;
-		if (resizeInput.checked) {
-			draw();
-		}
+		onResize();
 	}, false);
 
-	window.addEventListener('resize', function() {
-		console.log('onResize: ' + resizeInput.checked);
-		if (resizeInput.checked) {
-			draw();
-		}
-	}, false); // TODO: debounce
+	window.addEventListener('resize', onResize, false); // TODO: debounce
 
 	zoomInput.addEventListener('change', function() {
 		console.log('onChange: zoom: ' + zoomInput.value);
