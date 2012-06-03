9ea8178f97b5df8ef4333d672a3cfe6b1290c947
Peter Paul Elfferich <difosfor@gmail.com>
2012-06-03 21:10:07 +0200

    Added bar with links to select which function to draw with.

--- warping.js
+++ warping.js
@@ -4,16 +4,52 @@
 
 	'use strict';
 
+	// Constants
+	var fitToWindow = false;
 	var zoom = 5;
-	var octaves = 8;
+	var octaves = 4;
 
+	// Variables
 	var elem = document.getElementsByTagName('canvas')[0];
 	var context = elem.getContext('2d');
 	var canvas = context.canvas;
 
-	function draw() {
-		var w = window.innerWidth;
-		var h = window.innerHeight;
+	// Functions
+	function fbm(sx, sy) {
+		return fBm2d(sx, sy, octaves);
+	}
+
+	function fbm1(sx, sy) {
+		return fbm(sx, sy);
+	}
+
+	function fbm2(sx, sy) {
+		var qx = fbm(sx, sy);
+		var qy = fbm(sx + 5.2, sy + 1.3);
+		return fbm(sx + 4 * qx, sy + 4 * qy);
+	}
+
+	function fbm3(sx, sy) {
+		var qx = fbm(sx, sy);
+		var qy = fbm(sx + 5.2, sy + 1.3);
+		var rx = fbm(sx + 4 * qx + 1.7, sy + 4 * qy + 9.2);
+		var ry = fbm(sx + 4 * qx + 8.3, sy + 4 * qy + 2.8);
+		return fbm(sx + 4 * rx, sy + 4 * ry);
+	}
+
+	// convert the result from [-1, 1] to [0, 1] and to [0, 255]
+	function to255(v) {
+		return (v * 0.5 + 0.5) * 255;
+	}
+
+	function draw(f) {
+		f = f || window[window.localStorage.fn];
+		if (!f) {
+			return;
+		}
+
+		var w = fitToWindow ? window.innerWidth : canvas.width;
+		var h = fitToWindow ? window.innerHeight : canvas.height;
 		var imageData = context.createImageData(w, h);
 		var data = imageData.data;
 		var x, y;
@@ -25,33 +61,6 @@
 
 		function scaleY(y) {
 			return y / h * zoom;
-		}
-
-		// convert the result from [-1, 1] to [0, 1] and to [0, 255]
-		function to255(v) {
-			return (v * 0.5 + 0.5) * 255;
-		}
-
-		function fbm(sx, sy) {
-			return fBm2d(sx, sy, octaves);
-		}
-
-		function fbm1(sx, sy) {
-			return fbm(sx, sy);
-		}
-
-		function fbm2(sx, sy) {
-			var qx = fbm(sx, sy);
-			var qy = fbm(sx + 5.2, sy + 1.3);
-			return fbm(sx + 4 * qx, sy + 4 * qy);
-		}
-
-		function fbm3(sx, sy) {
-			var qx = fbm(sx, sy);
-			var qy = fbm(sx + 5.2, sy + 1.3);
-			var rx = fbm(sx + 4 * qx + 1.7, sy + 4 * qy + 9.2);
-			var ry = fbm(sx + 4 * qx + 8.3, sy + 4 * qy + 2.8);
-			return fbm(sx + 4 * rx, sy + 4 * ry);
 		}
 
 		function putPixel(x, y, r, g, b, a) {
@@ -70,19 +79,7 @@
 
 		for (y = 0; y < h; y++) {
 			for (x = 0; x < w; x++) {
-				var sx = scaleX(x);
-				var sy = scaleY(y);
-
-				// var v = fbm1(sx, sy);
-				// var v = fbm2(sx, sy);
-				var v = fbm3(sx, sy);
-
-				// var v = noise2d(sx, sy);
-				// var v = turbulence2d(sx, sy, octaves);
-				// var v = ridgedmf2d(sx, sy, octaves);
-
-				v = to255(v);
-
+				var v = to255(f(scaleX(x), scaleY(y), octaves));
 				putPixel(x, y, v, v, v);
 			}
 		}
@@ -93,14 +90,23 @@
 
 		var putDone = Date.now();
 
+		window.localStorage.setItem('fn', f.name);
+
 		window.console.log('Calc: ' + (dataDone - start) + ', Render: ' + (putDone - dataDone));
 	}
 
 	draw();
 
-	window.addEventListener('resize', draw, false);
+	if (fitToWindow) {
+		window.addEventListener('resize', draw, false);
+	}
 
 	window.ctx = context;
 	window.cnv = canvas;
+
+	window.fbm1 = fbm1;
+	window.fbm2 = fbm2;
+	window.fbm3 = fbm3;
+	window.draw = draw;
 
 }());
